b5dc734d4265b55e1516574b5e5155a42d0c4ef4
Yupeng Zhang <yzha3836@uni.sydney.edu.au>
2023-11-28 17:18:59 +1100

    Profile test on run_association test

---
+++ basic1.py
@@ -0,0 +1,19 @@
+# From https://limix.github.io/CellRegMap/usage.html
+
+from numpy import ones
+from numpy.random import RandomState
+
+from cellregmap import run_association, run_interaction, estimate_betas
+
+random = RandomState(10)
+n = 30                               # number of samples (cells)
+p = 5                                # number of individuals
+k = 4                                # number of contexts
+y = random.randn(n, 1)               # outcome vector (expression phenotype, one gene only)
+C = random.randn(n, k)               # context matrix (cells by contexts/factors)
+W = ones((n, 1))                     # intercept (covariate matrix)
+hK = random.randn(n, p)              # decomposition of kinship matrix (K = hK @ hK.T)
+g = 1.0 * (random.rand(n, 1) < 0.2)  # SNP vector
+
+## Association test
+pv0 = run_association(y=y, G=g, W=W, E=C, hK=hK)[0]
