d839ca539a3f6d0455bcee1a5e5b43ab9c2dec18
Yupeng Zhang <yzha3836@uni.sydney.edu.au>
2023-12-21 10:37:09 +1100

    change variants and add new log

--- more_data/100_variants_random.py
+++ more_data/100_variants_random.py
@@ -7,21 +7,20 @@ from limix.qc import quantile_gaussianize
 from cellregmap import run_association
 import itertools
 import cProfile, pstats, io
+import numpy as np
 
 plink_file = "../test_data/n.indep_100_n.cell_1.bed"
 G = read_plink1_bin(plink_file)
 
 txt_file = "../test_data/seed_1_100_nindep_100_ncell_100_lambda_2_tauIntraSample_0.5_Poisson.txt"
 txt_df = pd.read_csv(txt_file, sep="\t")
-# get the data for a1 in IND_ID
-txt_df_a1 = txt_df[txt_df["IND_ID"] == "a1"]
 
-pheno_df_a1 = txt_df_a1.iloc[:,5:]
+pheno_df = txt_df.iloc[:,5:]
 
-contexts_df_a1 = txt_df_a1[["X1","X2","pf1","pf2"]]
+contexts_df = txt_df[["X1","X2","pf1","pf2"]]
 
-txt_df_a1['cell'] = ["cell_" + str(val) for val in txt_df_a1.index.values]
-smf_df = txt_df_a1[["IND_ID","cell"]]
+txt_df['cell'] = ["cell_" + str(val) for val in txt_df.index.values]
+smf_df = txt_df[["IND_ID","cell"]]
 
 def get_groups_from_smf(smf_df):
     n_samples = smf_df.shape[0]
@@ -47,19 +46,21 @@ hK = get_block_hK_from_groups(groups)
 K = hK @ hK.T
 plt.matshow(K)
 
-C = xr.DataArray(contexts_df_a1.values, dims=('cell', 'pc'), coords={'cell': contexts_df_a1.index.values, 'pc': contexts_df_a1.columns.values})
+C = xr.DataArray(contexts_df.values, dims=('cell', 'pc'), coords={'cell': contexts_df.index.values, 'pc': contexts_df.columns.values})
 C = C.sel(cell=smf_df.index.values)
 C = quantile_gaussianize(C)
 
-y = xr.DataArray(pheno_df_a1.values, dims=["cell", "gene"], coords={"cell": pheno_df_a1.index.values, "gene": pheno_df_a1.columns.values})
+y = xr.DataArray(pheno_df.values, dims=["cell", "gene"], coords={"cell": pheno_df.index.values, "gene": pheno_df.columns.values})
 y = y.sel(gene="gene_1")
 y = y.values.reshape(y.shape[0],1)
 
-G_sel = xr.DataArray(G.values, dims=('sample', 'variant'), coords={'sample': G['sample'].values, 'variant': G['variant'].values})
+selected_variants = np.random.choice(G.variant.size, 100, replace=False)
+G_sel = G[:, selected_variants]
+G_sel = xr.DataArray(G_sel.values, dims=('sample', 'variant'), coords={'sample': G_sel['sample'].values, 'variant': G_sel['variant'].values})
 G_sel = G_sel.sel(sample=smf_df['IND_ID'].values)
 GG = G_sel.values
 
-W = ones((100,1))
+W = ones((10000,1))
 
 # turn profiling on
 pr = cProfile.Profile()
@@ -72,7 +73,7 @@ s = io.StringIO()
 sortby = 'cumulative'
 ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
 ps.print_stats()
-with open("a1_new.log", 'w') as f:
+with open("one_gene.log", 'w') as f:
     print(s.getvalue(), file=f)
 
 pv1 = pd.DataFrame({"sample":G_sel.sample.values,
